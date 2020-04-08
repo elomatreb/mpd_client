3dd4357d13080e5c110d73752caeb25346a09a3e
Ole Bertram <git@bertr.am>
2020-04-09 00:49:05 +0200

    Add utility methods for getting fields from frames

--- mpd_protocol/src/response.rs
+++ mpd_protocol/src/response.rs
@@ -379,6 +379,34 @@ impl Frame {
         }
     }
 
+    /// Find the first key-value pair with the given key, and return a reference to its value.
+    pub fn find<K>(&self, key: K) -> Option<&str>
+    where
+        K: AsRef<str>,
+    {
+        self.values
+            .iter()
+            .find(|&(k, _)| k.as_ref() == key.as_ref())
+            .map(|(_, v)| v.as_str())
+    }
+
+    /// Find the first key-value pair with the given key, and return its value.
+    ///
+    /// This removes it from the list of values in this frame.
+    pub fn get<K>(&mut self, key: K) -> Option<String>
+    where
+        K: AsRef<str>,
+    {
+        let index = self
+            .values
+            .iter()
+            .enumerate()
+            .find(|&(_, (k, _))| k.as_ref() == key.as_ref())
+            .map(|(index, _)| index);
+
+        index.map(|i| self.values.remove(i).1)
+    }
+
     /// Collect the key-value pairs in this resposne into a `HashMap`.
     ///
     /// Beware that this loses the order relationship between different keys. Values for a given
