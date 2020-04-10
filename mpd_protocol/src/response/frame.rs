0d38ec39d587f9d361624f75dcf3609f8ba3b7ec
Ole Bertram <git@bertr.am>
2020-04-10 02:09:39 +0200

    Add unit tests for Frame utility methods

--- mpd_protocol/src/response/frame.rs
+++ mpd_protocol/src/response/frame.rs
@@ -94,3 +94,77 @@ impl Frame {
 
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub(super) struct FieldsContainer(pub(super) Vec<Option<(Arc<str>, String)>>);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn length() {
+        let frame = Frame::empty();
+        assert_eq!(frame.fields_len(), 0);
+
+        let frame = Frame {
+            fields: FieldsContainer(vec![
+                Some((Arc::from("hello"), String::from("world"))),
+                Some((Arc::from("foo"), String::from("bar"))),
+            ]),
+            binary: Some(Vec::from("hello world")),
+        };
+
+        assert_eq!(frame.fields_len(), 2);
+    }
+
+    #[test]
+    fn binary() {
+        let mut frame = Frame {
+            fields: FieldsContainer::default(),
+            binary: Some(Vec::from("hello world")),
+        };
+
+        assert!(frame.has_binary());
+        assert!(!frame.is_empty());
+        assert_eq!(frame.get_binary(), Some(Vec::from("hello world")));
+        assert_eq!(frame.get_binary(), None);
+        assert!(!frame.has_binary());
+    }
+
+    #[test]
+    fn accessors() {
+        let mut frame = Frame {
+            fields: FieldsContainer(vec![
+                Some((Arc::from("hello"), String::from("first value"))),
+                Some((Arc::from("foo"), String::from("bar"))),
+                Some((Arc::from("hello"), String::from("second value"))),
+            ]),
+            binary: None,
+        };
+
+        assert_eq!(frame.find("hello"), Some("first value"));
+        assert_eq!(frame.find("404"), None);
+
+        assert_eq!(frame.get("hello"), Some(String::from("first value")));
+        assert_eq!(frame.get("hello"), Some(String::from("second value")));
+        assert_eq!(frame.get("hello"), None);
+    }
+
+    #[test]
+    fn iters() {
+        let frame = Frame {
+            fields: FieldsContainer(vec![
+                Some((Arc::from("hello"), String::from("first value"))),
+                Some((Arc::from("foo"), String::from("bar"))),
+                Some((Arc::from("hello"), String::from("second value"))),
+            ]),
+            binary: None,
+        };
+        let mut iter = frame.fields();
+
+        assert_eq!(iter.next(), Some(("hello", "first value")));
+        assert_eq!(iter.next(), Some(("foo", "bar")));
+        assert_eq!(iter.next(), Some(("hello", "second value")));
+
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
+    }
+}
