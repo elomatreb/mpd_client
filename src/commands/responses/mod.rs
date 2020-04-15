f758cc302db2ff59911ada829d081049bf7fabf3
Ole Bertram <git@bertr.am>
2020-04-15 06:20:33 +0200

    Move response field accesor macros to separate module

--- src/commands/responses/mod.rs
+++ src/commands/responses/mod.rs
@@ -1,5 +1,8 @@
 //! Typed responses to individual commands.
 
+#[macro_use]
+mod util_macros;
+
 use mpd_protocol::response::Frame;
 
 use std::convert::TryFrom;
@@ -71,79 +74,6 @@ impl TryFrom<Frame> for Empty {
         // silently ignore any actually existing fields
         Ok(Self)
     }
-}
-
-macro_rules! field {
-    ($frame:ident, $field:literal $type:ident) => {
-        field!($frame, $field $type optional)
-            .ok_or(TypedResponseError {
-                field: $field,
-                kind: ErrorKind::Missing,
-            })?
-    };
-    ($frame:ident, $field:literal $type:ident optional) => {
-        match $frame.get($field) {
-            None => None,
-            Some(val) => Some(parse!($type, val, $field))
-        }
-    };
-    ($frame:ident, $field:literal $type:ident default $default:expr) => {
-        field!($frame, $field $type optional).unwrap_or($default)
-    };
-}
-
-macro_rules! parse {
-    (integer, $value:ident, $field:literal) => {
-        $value.parse().map_err(|e| TypedResponseError {
-            field: $field,
-            kind: ErrorKind::MalformedInteger(e),
-        })?
-    };
-    (PlayState, $value:ident, $field:literal) => {
-        match $value.as_str() {
-            "play" => PlayState::Playing,
-            "pause" => PlayState::Paused,
-            "stop" => PlayState::Stopped,
-            _ => {
-                return Err(TypedResponseError {
-                    field: $field,
-                    kind: ErrorKind::InvalidValue($value),
-                })
-            }
-        }
-    };
-    (boolean, $value:ident, $field:literal) => {
-        match $value.as_str() {
-            "1" => true,
-            "0" => false,
-            _ => {
-                return Err(TypedResponseError {
-                    field: $field,
-                    kind: ErrorKind::InvalidValue($value),
-                })
-            }
-        }
-    };
-    (duration, $value:ident, $field:literal) => {
-        Duration::from_secs_f64($value.parse().map_err(|e| TypedResponseError {
-            field: $field,
-            kind: ErrorKind::MalformedFloat(e),
-        })?)
-    };
-}
-
-macro_rules! song_identifier {
-    ($frame:ident, $position:literal, $id:literal) => {
-        {
-            let pos = field!($frame, $position integer optional);
-            let id = field!($frame, $id integer optional);
-
-            match (pos, id) {
-                (Some(pos), Some(id)) => Some(SongIdentifier { pos, id }),
-                _ => None,
-            }
-        }
-    };
 }
 
 /// Identifier for a song in the queue, consisting of position-dependent index and stable ID.
---
+++ src/commands/responses/util_macros.rs
@@ -0,0 +1,73 @@
+macro_rules! field {
+    ($frame:ident, $field:literal $type:ident) => {
+        field!($frame, $field $type optional)
+            .ok_or(TypedResponseError {
+                field: $field,
+                kind: ErrorKind::Missing,
+            })?
+    };
+    ($frame:ident, $field:literal $type:ident optional) => {
+        match $frame.get($field) {
+            None => None,
+            Some(val) => Some(parse!($type, val, $field))
+        }
+    };
+    ($frame:ident, $field:literal $type:ident default $default:expr) => {
+        field!($frame, $field $type optional).unwrap_or($default)
+    };
+}
+
+macro_rules! parse {
+    (integer, $value:ident, $field:literal) => {
+        $value.parse().map_err(|e| TypedResponseError {
+            field: $field,
+            kind: ErrorKind::MalformedInteger(e),
+        })?
+    };
+    (PlayState, $value:ident, $field:literal) => {
+        match $value.as_str() {
+            "play" => PlayState::Playing,
+            "pause" => PlayState::Paused,
+            "stop" => PlayState::Stopped,
+            _ => {
+                return Err(TypedResponseError {
+                    field: $field,
+                    kind: ErrorKind::InvalidValue($value),
+                })
+            }
+        }
+    };
+    (boolean, $value:ident, $field:literal) => {
+        match $value.as_str() {
+            "1" => true,
+            "0" => false,
+            _ => {
+                return Err(TypedResponseError {
+                    field: $field,
+                    kind: ErrorKind::InvalidValue($value),
+                })
+            }
+        }
+    };
+    (duration, $value:ident, $field:literal) => {
+        Duration::from_secs_f64($value.parse().map_err(|e| TypedResponseError {
+            field: $field,
+            kind: ErrorKind::MalformedFloat(e),
+        })?)
+    };
+}
+
+macro_rules! song_identifier {
+    ($frame:ident, $position:literal, $id:literal) => {
+        {
+            let pos = field!($frame, $position integer optional);
+            let id = field!($frame, $id integer optional);
+
+            match (pos, id) {
+                (Some(pos), Some(id)) => Some(SongIdentifier { pos, id }),
+                _ => None,
+            }
+        }
+    };
+}
+
