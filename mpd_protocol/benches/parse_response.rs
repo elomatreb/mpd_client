015b1da474b8ab83094a288d5f0bcfc0432a343a
Ole Bertram <git@bertr.am>
2025-08-10 15:25:59 +0200

    Fix formatting

--- mpd_protocol/benches/parse_response.rs
+++ mpd_protocol/benches/parse_response.rs
@@ -1,7 +1,7 @@
+use std::hint::black_box;
+
 use criterion::{Criterion, criterion_group, criterion_main};
 use mpd_protocol::Connection;
-
-use std::hint::black_box;
 
 // NOTE: Benchmark requires `--cfg criterion` to be set to build correctly.
 
